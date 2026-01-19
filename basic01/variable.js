55e6a9ab60e170d35ce08592194be58d997df0d8
notchvijay <krishnaxnotch@gmail.com>
2026-01-19 16:45:17 +0000

    conifg

---
+++ basic01/variable.js
@@ -0,0 +1,6 @@
+const accountId = 99990
+let accountEmail = "javascript@gmail.com"
+let accountPass = "11223344"
+accountLocation = "uttrakhand"
+
+console.table([accountId,accountEmail,accountPass,accountLocation])
