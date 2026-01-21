7dd47cc1e710c698f74a12ff74ce0c29cecf6127
notchvijay <krishnaxnotch@gmail.com>
2026-01-21 11:30:12 +0000

    conversion

---
+++ basic01/conversionoperation.js
@@ -0,0 +1,29 @@
+let score="33"
+
+console.log(typeof score)
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof (valueInNumber))
+
+// "33"=> number 33
+// "33abc"=> NaN
+// true = 1, false = 0
+
+let loggedIn = 1
+console.log(typeof loggedIn)
+console.log(loggedIn)
+
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(booleanLoggedIn)
+
+// conversion in boolean
+// 1 => true;  0 => false 
+// "" => false 
+// "notch" => true
+
+let number = 44
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
