521773cb22bc74ff690d07f92344110dea21cf7a
notchvijay <krishnaxnotch@gmail.com>
2026-01-21 13:10:03 +0000

    conversion

--- basic01/conversionoperation.js
+++ basic01/conversionoperation.js
@@ -4,7 +4,6 @@ console.log(typeof score)
 console.log(typeof (score));
 
 let valueInNumber = Number(score);
-
 console.log(typeof (valueInNumber))
 
 // "33"=> number 33
@@ -27,3 +26,35 @@ let number = 44
 let stringNumber = String(number)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+//      *************************************************operation**************************************************************
+
+let value = 5
+let NegValue = -value
+console.log(NegValue)
+
+console.log(2+2);  // addition
+console.log(2-2);  // subtraction
+console.log(2*2) // multiplication  
+console.log(2**3) // 2 to the power 2
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+3)
+console.log(1+2+"3")
+
+
+ console.log(true)
+ console.log(+true)
+ console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3= 2+2
+console.log(num1)
+
+
+let gamecounter = 100
+gamecounter++;
+++gamecounter;
+console.log(gamecounter);
