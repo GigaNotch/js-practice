d84b1ca8587f3732cf6deb827b29665c7f943d27
notchvijay <krishnaxnotch@gmail.com>
2026-01-25 17:41:45 +0000

    numbers

---
+++ basic01/7num_and_maths.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3))
+
+const newnumber=1412.58669
+console.log(newnumber.toPrecision(3));
+
+const Hundreds=10000000
+console.log(Hundreds.toLocaleString('en-In'));
+
+//+++++++++++++++++++++++++++++++++  Maths  ++++++++++++++++++++++++++++++++++++++++++
+
