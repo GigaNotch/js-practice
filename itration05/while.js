0bb38f25a65e7618a5782cc5352852c96903c7b1
notch <krishnaxnotch@gmail.com>
2026-02-04 10:22:40 +0530

    while ,do while

---
+++ itration05/while.js
@@ -0,0 +1,19 @@
+let i=0
+while(i<11){
+    // console.log(`value of i ${i}`)
+    i=i+2
+}
+
+let myarray=["notch","viajay","jaggu","raju"]
+// let i=0;
+while(i<myarray.length){
+    // console.log(myarray[i])
+    i++
+}
+
+
+let score=11
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score<10); 
